bfcc305a77dfed4ecc5b0613353e9672e5fc5cf9
331672stud <01187521@pw.edu.pl>
2023-12-20 20:56:53 +0100

    gauss z elem gl, usuniecie dzielenia rown 1.

--- src/gauss.c
+++ src/gauss.c
@@ -7,19 +7,31 @@
 int eliminate(Matrix *mat, Matrix *b){
 	double dzielnik;
 	double wielk;
-	for(int i=0;i<mat->c;i++){
+	double maks=0;
+	int mrzad;
+	double kopia;
+	for(int i=0;i<mat->r;i++){
+		for(int j=i;j<mat->r;j++){//znajduje maks a i zapisuje "lokacje"
+			if(mat->data[j][i]>maks){
+				mrzad=j;
+				maks=mat->data[j][i];
+			}
+		}
+		if(mrzad!=i){//jesli maks nie w schodkowo pierwszej linii
+			for(int j=0;j<c;j++){//to zamien je miejscami
+				kopia=mat->data[i][j];
+				mat->data[i][j]=mat->data[mrzad][j];
+				mat->data[mrzad][j]=kopia;
+			}
+		}
 		dzielnik=mat->data[i][i]; //dzielnik=a dla ax
 		if(dzielnik==0)
-			return 1; //czyosobliwa
-		b->data[i][0]/=dzielnik; //dzielenie wyniku na start
-		for(int j=i;j<mat->r;j++){//normalizacja rownania
-			mat->data[i][j]/=dzielnik;
-		}
+			return 1; //czyosobliwa(niepowinno byc po znalezeniu elem gl.
 		for(int j=i+1;j<mat->c;j++){//petla przechodzi po kolejnych rownaniach
-			wielk=mat->data[j][i];
-			b->data[j][0]-=b->data[i][0]*wielk;	//odejmowanie wyniku
+			wielk=mat->data[j][i]; //wielk=ax dla kolejnych rownan
+			b->data[j][0]-=b->data[i][0]*wielk/dzielnik;	//odejmowanie wyniku
 			for(int k=i;k<mat->r;k++){//odejmowanie reszty
-				mat->data[j][k]-=mat->data[i][k]*wielk;		
+				mat->data[j][k]-=mat->data[i][k]*wielk/dzielnik;		
 			}
 		}
 	}
