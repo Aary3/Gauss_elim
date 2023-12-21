7674ce4de8d75bdbc0f699a13692120a18423d79
aary <piotrsosnowskidev@onet.pl>
2023-12-21 16:04:18 +0100

    Dodalem wybor elementu

--- src/gauss.c
+++ src/gauss.c
@@ -6,21 +6,28 @@
  */
 int eliminate(Matrix *mat, Matrix *b){
 	double dzielnik;
-	double wielk;
-	for(int i=0;i<mat->c;i++){
-		dzielnik=mat->data[i][i]; //dzielnik=a dla ax
-		if(dzielnik==0)
-			return 1; //czyosobliwa
-		b->data[i][0]/=dzielnik; //dzielenie wyniku na start
-		for(int j=i;j<mat->r;j++){//normalizacja rownania
-			mat->data[i][j]/=dzielnik;
+	for(int i=0;i<mat->c-1;i++){
+		int max = i;
+		for(int j=i+1;j<mat->r;j++){//normalizacja rownania
+			if (mat->data[j][i] > mat->data[max][i] )
+				max = j;
 		}
-		for(int j=i+1;j<mat->c;j++){//petla przechodzi po kolejnych rownaniach
-			wielk=mat->data[j][i];
-			b->data[j][0]-=b->data[i][0]*wielk;	//odejmowanie wyniku
+		if (max != i ) {			//znajdywanie najwiekszej liczby
+			double *tmp = mat->data[i];
+			mat->data[i] = mat->data[max];
+			mat->data[max] = tmp;
+			double bt = b->data[max][0];
+			b->data[max][0] = b->data[i][0];
+			b->data[i][0] = bt;
+		}
+		for(int j=i+1;j<mat->c;j++){//petla przechodzi po kolejnych rownaniach wielk=mat->data[j][i];
+			if (mat->data[i][i] == 0)
+				return 1;	//dzielenie przez 0
+			dzielnik = mat->data[j][i] / mat->data[i][i];
 			for(int k=i;k<mat->r;k++){//odejmowanie reszty
-				mat->data[j][k]-=mat->data[i][k]*wielk;		
+				mat->data[j][k]-=mat->data[i][k]*dzielnik;		
 			}
+			b->data[j][0] -= b->data[i][0]*dzielnik;
 		}
 	}
 	return 0;
